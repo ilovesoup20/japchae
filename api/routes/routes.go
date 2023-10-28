da99ecada021e328eda9abc558817d22b9d1b101
Charles Chong <ilovesoup@fastmail.com>
2023-10-28 16:15:09 +0900

    Add routes for todo

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -8,4 +8,12 @@ import (
 func SetupRoutes(app *fiber.App) {
 
 	app.Get("/hello", controllers.Hello)
+
+	// TODO
+	app.Get("/todo", controllers.ListTodo)
+	app.Get("/todo/:id", controllers.GetTodoByID)
+	app.Post("/todo", controllers.CreateTodo)
+	app.Put("/todo/:id", controllers.UpdateTodo)
+	app.Patch("/todo/:id", controllers.PatchTodo)
+	app.Delete("/todo/:id", controllers.DeleteTodoById)
 }
