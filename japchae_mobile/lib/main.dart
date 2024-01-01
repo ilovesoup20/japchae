383e3b3a7fa969991f4c596b19e5f73c459ed672
Charles Chong <ilovesoup@fastmail.com>
2024-01-01 17:57:07 +0900

    Delete all practice code. (Moved to flutter_dojo)

--- japchae_mobile/lib/main.dart
+++ japchae_mobile/lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:japchae_mobile/samples/basics/03_lists_and_grids/02_horizontal_list.dart';
-import 'package:japchae_mobile/samples/basics/03_lists_and_grids/03_grid_view.dart';
-import 'package:japchae_mobile/samples/codelab_sample/sample1.dart';
-import 'package:japchae_mobile/samples/udemy/login_stateful/app.dart';
-import 'package:japchae_mobile/samples/udemy/news/news_app.dart';
-import 'package:japchae_mobile/samples/udemy/pics_app/pics_app.dart';
 
 void main() {
-  runApp(GridViewApp());
+  runApp();
 }
--- japchae_mobile/lib/samples/basics/01_introduction/01_hello_world.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class HelloWorldApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext ctx) {
-    return Center(
-        child: Text('Hello world!', textDirection: TextDirection.ltr));
-  }
-}
